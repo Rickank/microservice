f4e8db6a5c1d6bdc0d3d830cfe4fe9288cd63e98
Rickard Ankar <rickard.ankar@iths.se>
2026-05-25 15:32:01 +0200

    feat: user-service with CRUD and H2 database

---
+++ user-service/src/main/java/com/microchat/user_service/UserController.java
@@ -0,0 +1,35 @@
+package com.microchat.user_service;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+}
